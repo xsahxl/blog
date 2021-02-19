875c9598e89290613365226f46034eadc1790ba1
wb-shl391697 <wb-shl391697@alibaba-inc.com>
2021-02-19 15:26:21 +0800

    add main=>master

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:xsahxl/xsahxl.github.io.git master
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:xsahxl/blog.git main:gh-pages
+git push -f git@github.com:xsahxl/blog.git master:gh-pages
 
 cd -

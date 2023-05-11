60597dc96c5253d1045fe06b72e462ec2027ae62
xsahxl <xsahxl@126.com>
2023-05-11 21:24:01 +0800

    test: ✅ debug
    Signed-off-by: xsahxl <xsahxl@126.com>

--- src/tag-container/index.tsx
+++ src/tag-container/index.tsx
@@ -62,7 +62,8 @@ const TagContainerWithPure: FC<Props> = (props) => {
     ? [{ id: uniqueId() }]
     : concat(map(value, (item) => ({ ...item, id: uniqueId() })), { id: uniqueId() });
   const [list, setList] = useState<IItem[]>(defaultValue);
-  const { data } = useRequest(fetchData)
+  const { data } = useRequest(fetchData);
+  console.log(data, 'data')
 
   const handleDelete = (id: string) => {
     const option = filter(list, (item) => item.id !== id);
